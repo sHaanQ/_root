fcb92e51926beefba07b1f8a579331639b65fbf6
bhargav.ak <anur.bhargav@gmail.com>
2015-07-12 22:50:28 +0530

    Cleaning up sources

--- bare_bones/ascii_hex.c
+++ bare_bones/ascii_hex.c
@@ -1,9 +1,21 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ **/
 
 #include <stddef.h>
 #include <stdint.h>
+
 /*
- * Get a Two byte input in ASCII
- * convert it to Binary coded Hexadecimal
+ * @func: Get a Two byte input in ASCII convert it to Binary coded Hexadecimal
  *
  * @ascii_text - Two byte ASCII value
  * @bcd_value - Length equivalent of the ASCII input
---
+++ bare_bones/uart.h
@@ -0,0 +1,65 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef UART_H
+#define UART_H
+
+enum address_map
+{
+    // The GPIO registers base address.
+    GPIO_BASE = 0x20200000,
+ 
+    // The offsets for reach register.
+ 
+    // Controls actuation of pull up/down to ALL GPIO pins.
+    GPPUD = (GPIO_BASE + 0x94),
+ 
+    // Controls actuation of pull up/down for specific GPIO pin.
+    GPPUDCLK0 = (GPIO_BASE + 0x98),
+ 
+    // The base address for UART.
+    UART0_BASE = 0x20201000,
+ 
+    // The offsets for reach register for the UART.
+    UART0_DR     = (UART0_BASE + 0x00),
+    UART0_RSRECR = (UART0_BASE + 0x04),
+    UART0_FR     = (UART0_BASE + 0x18),
+    UART0_ILPR   = (UART0_BASE + 0x20),
+    UART0_IBRD   = (UART0_BASE + 0x24),
+    UART0_FBRD   = (UART0_BASE + 0x28),
+    UART0_LCRH   = (UART0_BASE + 0x2C),
+    UART0_CR     = (UART0_BASE + 0x30),
+    UART0_IFLS   = (UART0_BASE + 0x34),
+    UART0_IMSC   = (UART0_BASE + 0x38),
+    UART0_RIS    = (UART0_BASE + 0x3C),
+    UART0_MIS    = (UART0_BASE + 0x40),
+    UART0_ICR    = (UART0_BASE + 0x44),
+    UART0_DMACR  = (UART0_BASE + 0x48),
+    UART0_ITCR   = (UART0_BASE + 0x80),
+    UART0_ITIP   = (UART0_BASE + 0x84),
+    UART0_ITOP   = (UART0_BASE + 0x88),
+    UART0_TDR    = (UART0_BASE + 0x8C),
+};
+
+struct frame {
+    uint8_t start_byte;
+    uint16_t data_length;
+    uint8_t *data;
+    uint8_t crc;
+};
+
+uint32_t encode_base_64(uint8_t *encoded, const char *src, uint32_t len);
+uint8_t ascii_to_length(uint8_t *ascii_text);
+
+#endif
+
