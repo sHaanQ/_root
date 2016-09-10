6986ae27157dec31f8634063b657da7fcf9849c8
Bhargav AK <anur.bhargav@gmail.com>
2016-09-11 01:43:48 +0530

    [Scripts] - Added kernel cleanup script
    Helper script to clean up old installed kernel on my ubuntu machine

---
+++ scripts/kernel-cleanup.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Remove following files/dirs
+
+function usage(){
+	echo "Usage: $0 kernel-version"
+	echo "Ex: $0 4.6"
+	exit 1
+}
+
+function cleanup(){
+
+	local VERSION=$1
+	sudo echo "Cleaning all $VERSION version's of linux"
+
+	echo "boot/vmlinux"
+	sudo rm /boot/vmlinuz-$VERSION*
+	if [ $? -eq 0 ]; then echo CLEANED; else echo SKIPPED; fi
+
+	echo "boot/initrd.img"
+	sudo rm /boot/initrd.img-$VERSION*
+	if [ $? -eq 0 ]; then echo CLEANED; else echo SKIPPED; fi
+
+	echo "boot/System.map"
+	sudo rm /boot/System.map-$VERSION*
+	if [ $? -eq 0 ]; then echo CLEANED; else echo SKIPPED; fi
+
+	echo "boot/config"
+	sudo rm /boot/config-$VERSION*
+	if [ $? -eq 0 ]; then echo CLEANED; else echo SKIPPED; fi
+
+	echo "lib/modules"
+	if [ ! -z `ls -l /lib/modules/$VERSION*`]; then
+		sudo rm -rf /lib/modules/$VERSION*
+		echo "CLEANED"
+	else
+		echo "SKIPPED"
+		break;
+	fi
+		
+	echo "updating grub"
+	sudo update-grub
+}
+
+if [ -z "$1" ]; then
+	echo "No argument supplied"
+	usage
+	exit 1
+fi 
+
+while true; do
+    read -p "This script can brick ur system, Do you wish to continue? " yn
+    case $yn in
+        [Yy]* ) cleanup "$1"; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
