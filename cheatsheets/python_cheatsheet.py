7246ce268c883f1bc940597819d8676ad615804d
Bhargav <bhargav89@users.noreply.github.com>
2016-10-10 00:47:44 +0530

    Update python_cheatsheet.py

--- cheatsheets/python_cheatsheet.py
+++ cheatsheets/python_cheatsheet.py
@@ -22,6 +22,17 @@
 			return "Can you speak up?"
 
 	shout("I'M INTERESTED IN SHOUTING")
+	
+	def count_small(numbers):
+	    total = 0
+	    for n in numbers:
+		if n < 10:
+		    total = total + 1
+	    return total
+
+	lost = [4, 8, 15, 16, 23, 42]
+	small = count_small(lost)
+	print small
 
 
 #[Function Imports]
