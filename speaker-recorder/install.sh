3a186bb664e4e414176588a12c8a0532b3bd4c19
Bhargav AK <anur.bhargav@gmail.com>
2017-02-19 14:13:34 +0530

    [Speaker][Recorder] - Fixed broken application and paths

--- speaker-recorder/install.sh
+++ speaker-recorder/install.sh
@@ -3,6 +3,6 @@
 sudo echo "Installing pulseaudio-utils and lame"
 sudo apt-get install pulseaudio-utils lame
 
-mkdir ~/.local/share/applications/record-speakers
-cp record_speakers.py record-spreakers.svg record_speakers.glade record_speakers.desktop ~/.local/share/applications/record-speakers
-
+sudo mkdir /usr/local/bin/record-speakers
+sudo cp record_speakers.py record-spreakers.svg record_speakers.glade /usr/local/bin/record-speakers
+cp record_speakers.desktop ${HOME}/.local/share/applications/
