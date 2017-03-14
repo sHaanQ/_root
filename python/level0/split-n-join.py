03f52aa4456c27e2000edd062e2b38bd10327351
Bhargav <bhargav89@users.noreply.github.com>
2017-03-14 17:03:11 +0530

    Update split-n-join.py

--- python/level0/split-n-join.py
+++ python/level0/split-n-join.py
@@ -1,8 +1,9 @@
 def split_and_join(line):
-    # write your code here
     line = line.split()
+    print "line is now a list chopped at spaces ->", line
     line = "-".join(line)
     return line
 print split_and_join("This is the end")
 
+# => line is now a list chopped at spaces -> ['This', 'is', 'the', 'end']
 # => This-is-the-end
