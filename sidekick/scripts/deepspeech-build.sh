69c538253901fe4ee33bc07b2f191343024f8c1d
Bhargav A K <anur.bhargav@gmail.com>
2018-08-19 12:44:47 +0530

    Corrections to Deepspeech Builder

--- sidekick/scripts/deepspeech-build.sh
+++ sidekick/scripts/deepspeech-build.sh
@@ -55,10 +55,10 @@ echo "--------------------------------------------------------------------"
 select yn in "Yes" "No"; do
 	case $yn in
 		echo "Check Username, password and proxy URL"
-		Yes ) ~/rpi/nlp/tensorflow$ unset http_proxy 
-		      ~/rpi/nlp/tensorflow$ unset https_proxy 
-		      ~/rpi/nlp/tensorflow$ export HTTPS_PROXY=https://username:pass@proxy-server:port
-		      ~/rpi/nlp/tensorflow$ export HTTPS_PROXY=https://username:pass@proxy-server:port
+		Yes ) unset http_proxy 
+		      unset https_proxy 
+		      export HTTPS_PROXY=https://username:pass@proxy-server:port
+		      export HTTPS_PROXY=https://username:pass@proxy-server:port
 			  break;;
 		No  ) break;;
 	esac
@@ -66,34 +66,29 @@ done
 
 
 
-echo "--------------------------------------------------------------------"
-echo "Building with AVX AVX2 FMA SSE4.1 SSE4.2"
-echo "Check using : cat /proc/cpuinfo"
-echo "--------------------------------------------------------------------"
-select yn in "Yes" "No"; do
-	# If AVX2, FMA support present on CPU
-	case $yn in
-		Yes ) time bazel build --jobs 4 -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma \
-				--copt=-msse4.1 --copt=-msse4.2 //tensorflow/tools/pip_package:build_pip_package
-			  break;
-		# If No AVX2 support, fallback to deafult config options
-		No )  bazel build --jobs 4 --config=opt //tensorflow/tools/pip_package:build_pip_package
-			  break;
-
-
-echo "Build TensorFlow pip package and install it"
-time bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
-pip install --user /tmp/tensorflow_pkg/tensorflow-1.10.0-py2-none-any.whl
-
-
-echo "--------------------------------------------------------------------"
-echo "Test the build's working"
-echo "Change directory (cd) to any directory on your system other"
-echo "than the tensorflow subdirectory from which you invoked the configure command"
-echo "Try a sample tensorflow program"
-echo "--------------------------------------------------------------------"
-cd ../tensorflow
-# Call tensorflow.py
+#echo "--------------------------------------------------------------------"
+#echo "Building with AVX AVX2 FMA SSE4.1 SSE4.2"
+#echo "Check using : cat /proc/cpuinfo"
+#echo "--------------------------------------------------------------------"
+#select yn in "Yes" "No"; do
+#	# If AVX2, FMA support present on CPU
+#	case $yn in#		Yes ) bazel build --jobs 4 -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma \
+#				--copt=-msse4.1 --copt=-msse4.2 //tensorflow/tools/pip_package:build_pip_package
+#			  break;
+#		# If No AVX2 support, fallback to deafult config options#		No )  bazel build --jobs 4 --config=opt //tensorflow/tools/pip_package:build_pip_package
+#			  break;
+#
+#
+#echo "Build TensorFlow pip package and install it"
+#bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+#pip install --user /tmp/tensorflow_pkg/tensorflow-1.10.0-py2-none-any.whl
+#echo "--------------------------------------------------------------------"
+#echo "Test the build's working"
+#echo "Change directory (cd) to any directory on your system other"
+#echo "than the tensorflow subdirectory from which you invoked the configure command"
+#echo "Try a sample tensorflow program"
+#echo "--------------------------------------------------------------------"
+#cd ../tensorflow## Call tensorflow.py
 
 
 echo "--------------------------------------------------------------------"
@@ -102,8 +97,11 @@ echo "Before building the DeepSpeech client libraries, you will need to prepare 
 echo "environment to configure and build TensorFlow."
 echo "--------------------------------------------------------------------"
 cd ./tensorflow
-bazel build -c opt --copt=-O3 --copt="-D_GLIBCXX_USE_CXX11_ABI=0" //native_client:libctc_decoder_with_kenlm.so
-bazel build --config=monolithic -c opt --copt=-O3 --copt="-D_GLIBCXX_USE_CXX11_ABI=0" --copt=-fvisibility=hidden //native_client:libdeepspeech.so //native_client:generate_trie
+bazel build -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-msse4.1 --copt=-msse4.2 --copt=-O3 --copt="-D_GLIBCXX_USE_CXX11_ABI=0" //native_client:libctc_decoder_with_kenlm.so
+bazel build --config=monolithic -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-msse4.1 --copt=-msse4.2 --copt=-O3 --copt="-D_GLIBCXX_USE_CXX11_ABI=0" --copt=-fvisibility=hidden //native_client:libdeepspeech.so //native_client:generate_trie
+
+# All generated libs @ location 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/bhargav/Coder/nlp/tensorflow/bazel-bin/native_client
 
 cd ../DeepSpeech/native_client
 echo "--------------------------------------------------------------------"
