f236c7b9e4a806d166cea92102ef00bff9534ba7
Bhargav A K <anur.bhargav@gmail.com>
2018-08-24 00:40:28 +0530

    Corrections to Deepspeech Builder

--- sidekick/scripts/deepspeech-build.sh
+++ sidekick/scripts/deepspeech-build.sh
@@ -66,33 +66,6 @@ select yn in "Yes" "No"; do
 	esac
 done
 
-
-
-#echo "--------------------------------------------------------------------"
-#echo "Building with AVX AVX2 FMA SSE4.1 SSE4.2"
-#echo "Check using : cat /proc/cpuinfo"
-#echo "--------------------------------------------------------------------"
-#select yn in "Yes" "No"; do
-#	# If AVX2, FMA support present on CPU
-#	case $yn in#		Yes ) bazel build --jobs 4 -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma \
-#				--copt=-msse4.1 --copt=-msse4.2 //tensorflow/tools/pip_package:build_pip_package
-#			  break;
-#		# If No AVX2 support, fallback to deafult config options#		No )  bazel build --jobs 4 --config=opt //tensorflow/tools/pip_package:build_pip_package
-#			  break;
-#
-#
-#echo "Build TensorFlow pip package and install it"
-#bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
-#pip install --user /tmp/tensorflow_pkg/tensorflow-1.10.0-py2-none-any.whl
-#echo "--------------------------------------------------------------------"
-#echo "Test the build's working"
-#echo "Change directory (cd) to any directory on your system other"
-#echo "than the tensorflow subdirectory from which you invoked the configure command"
-#echo "Try a sample tensorflow program"
-#echo "--------------------------------------------------------------------"
-#cd ../tensorflow## Call tensorflow.py
-
-
 echo "--------------------------------------------------------------------"
 echo "Get back to building DeepSpeech"
 echo "Before building the DeepSpeech client libraries, you will need to prepare your"
@@ -108,19 +81,25 @@ cd ./tensorflow
 
 # Build LM Prefix Decoder, CPU only - no need for CUDA flag
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/:/home/bhargav/Coder/nlp/tensorflow/bazel-bin/native_client
-bazel build --jobs 4 -c opt --copt=-O3 --copt="-D_GLIBCXX_USE_CXX11_ABI=0" --copt=-mtune=generic --copt=-march=x86-64 --copt=-msse --copt=-msse2 --copt=-msse3 --copt=-msse4.1 --copt=-msse4.2 --copt=-mavx //native_client:libctc_decoder_with_kenlm.so  --verbose_failures --action_env=LD_LIBRARY_PATH=${LD_LIBRARY_PATH}
-#bazel build -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-msse4.1 --copt=-msse4.2 --copt=-O3 --copt="-D_GLIBCXX_USE_CXX11_ABI=0" //native_client:libctc_decoder_with_kenlm.so
+
+bazel build --jobs 4 -c opt --copt=-O3 --copt="-D_GLIBCXX_USE_CXX11_ABI=0" --copt=-mtune=generic --copt=-march=x86-64 \
+	--copt=-msse --copt=-msse2 --copt=-msse3 --copt=-msse4.1 --copt=-msse4.2 --copt=-mavx --copt=-mavx2 --copt=-mfma \
+	//native_client:libctc_decoder_with_kenlm.so  --verbose_failures --action_env=LD_LIBRARY_PATH=${LD_LIBRARY_PATH}
 
 # Build DeepSpeech
-bazel build --jobs 4 --config=monolithic  -c opt --copt=-O3 --copt="-D_GLIBCXX_USE_CXX11_ABI=0" --copt=-mtune=generic --copt=-march=x86-64 --copt=-msse --copt=-msse2 --copt=-msse3 --copt=-msse4.1 --copt=-msse4.2 --copt=-mavx --copt=-fvisibility=hidden //native_client:libdeepspeech.so //native_client:deepspeech_utils //native_client:generate_trie --verbose_failures --action_env=LD_LIBRARY_PATH=${LD_LIBRARY_PATH}
-#bazel build --config=monolithic -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-msse4.1 --copt=-msse4.2 --copt=-O3 --copt="-D_GLIBCXX_USE_CXX11_ABI=0" --copt=-fvisibility=hidden //native_client:libdeepspeech.so //native_client:generate_trie
+bazel build --jobs 4 --config=monolithic  -c opt --copt=-O3 --copt="-D_GLIBCXX_USE_CXX11_ABI=0" --copt=-mtune=generic \
+	--copt=-march=x86-64 --copt=-msse --copt=-msse2 --copt=-msse3 --copt=-msse4.1 --copt=-msse4.2 --copt=-mavx \
+	--copt=-mavx2 --copt=-mfma --copt=-fvisibility=hidden //native_client:libdeepspeech.so //native_client:deepspeech_utils \
+	//native_client:generate_trie --verbose_failures --action_env=LD_LIBRARY_PATH=${LD_LIBRARY_PATH}
 
 # Build Tensorflow
 ln -s ../DeepSpeech/native_client/ ./
 
 # Build TF pip package
 ./configure
-bazel build --jobs 4 --config=opt --copt="-D_GLIBCXX_USE_CXX11_ABI=0" --copt=-mtune=generic --copt=-march=x86-64 --copt=-msse --copt=-msse2 --copt=-msse3 --copt=-msse4.1 --copt=-msse4.2 --copt=-mavx //tensorflow/tools/pip_package:build_pip_package --verbose_failures --action_env=LD_LIBRARY_PATH=${LD_LIBRARY_PATH}
+bazel build --jobs 4 --config=opt --copt="-D_GLIBCXX_USE_CXX11_ABI=0" --copt=-mtune=generic --copt=-march=x86-64 --copt=-msse \
+	 --copt=-msse2 --copt=-msse3 --copt=-msse4.1 --copt=-msse4.2 --copt=-mavx --copt=-mavx2 --copt=-mfma \
+	 //tensorflow/tools/pip_package:build_pip_package --verbose_failures --action_env=LD_LIBRARY_PATH=${LD_LIBRARY_PATH}
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
 pip install /tmp/tensorflow_pkg/*.whl
 
